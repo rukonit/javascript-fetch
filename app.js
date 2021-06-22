e3aff1a4c48b27709cb85caa5945acfe537638a1
Rukon Khan <56213291+rukonit@users.noreply.github.com>
2021-06-21 23:06:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-fetch('https://api.cryptonator.com/api/ticker/btc-usd').then(res => {console.log('Waiting to parse');
+const firstWay = fetch('https://api.cryptonator.com/api/ticker/btc-usd').then(res => {console.log('Waiting to parse');
 return res.json();}).then((response)=>{
     console.log(response.ticker);
 })
 .catch((e)=>{
     console.log(e);
 })
+
+
+const fetchBitcoinPrice = async () => {
+    const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
+    const data = await res.json();
+    console.log(data.ticker.price);
+}
